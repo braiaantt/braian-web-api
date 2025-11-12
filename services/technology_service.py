6b8428481feb2f6dfa03c8e8e7a25da443791d44
Braian Torres <brianntorress.s@gmail.com>
2025-11-12 17:15:00 -0300

    feat(services): add TechnologyService class

---
+++ services/technology_service.py
@@ -0,0 +1,48 @@
+from sqlmodel import Session
+from daos import TechnologyDao
+from database import Technology
+from models import TechnologyUpdate
+from exceptions import TechnologyNotExists, TechnologyDeletingError, TechnologyCreationError, TechnologyUpdatingError
+
+class TechnologyService:
+    def __init__(self, session: Session):
+        self.technologyDao = TechnologyDao(session)
+
+    def insert_technology(self, technology: Technology):
+        technology_created = self.technologyDao.insert_technology(technology)
+
+        if not technology_created:
+            raise TechnologyCreationError()
+        
+        return technology_created
+
+    def update_technology(self, tech_id: int, data: TechnologyUpdate):
+        technology = self.technologyDao.get_tech(tech_id)
+
+        if not technology:
+            raise TechnologyNotExists()
+        
+        update_data = data.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(technology, key, value)
+
+        technology_updated = self.technologyDao.update_tech(technology)
+
+        if not technology_updated:
+            raise TechnologyUpdatingError()
+        
+        return technology_updated
+
+
+    def delete_technology(self, tech_id):
+        exists = self.technologyDao.get_tech(tech_id)
+
+        if not exists:
+            raise TechnologyNotExists()
+        
+        result = self.technologyDao.delete_tech(tech_id)
+
+        if not result:
+            raise TechnologyDeletingError()
+        
+        return True
