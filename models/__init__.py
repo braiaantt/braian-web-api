460cbd9b1cb268abcb1496c7dbfd34b9230d895b
Braian Torres <brianntorress.s@gmail.com>
2025-11-12 17:06:26 -0300

    feat(models) add import from 'technology.py' to '__init__.py'

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .project import PortfolioProject
 from .portfolio import (PortfolioProject,
                        PortfolioRead,
                        PortfolioUpdate)
+from .technology import TechnologyUpdate
