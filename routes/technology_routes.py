9c34f2c19d9a827c6d4649ffb433f4888dcad630
Braian Torres <brianntorress.s@gmail.com>
2025-11-12 17:18:37 -0300

    feat(routes): add API router for Technology resource

---
+++ routes/technology_routes.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException
+from database import get_session, Technology
+from models import TechnologyUpdate
+from services import TechnologyService
+from exceptions import TechnologyNotExists, TechnologyCreationError, TechnologyUpdatingError
+
+router = APIRouter()
+
+@router.post("/technology", status_code=201)
+def insert_technology(technology: Technology, session = Depends(get_session)):
+    service = TechnologyService(session)
+    try:
+        new_technology = service.insert_technology(technology)
+
+        if new_technology:
+            return {"success" : True,
+                    "data" : new_technology}
+    
+    except TechnologyCreationError:
+        raise HTTPException(status_code=500, detail="Database Error Creating Technology")
+
+
+@router.put("/technology/{tech_id}", status_code=200)
+def update_technology(tech_id: int, data: TechnologyUpdate, session = Depends(get_session)):
+    service = TechnologyService(session)
+    try:
+        updated_technology = service.update_technology(tech_id, data)
+        
+        if updated_technology:
+            return {"success" : True,
+                    "data" : updated_technology}
+
+    except TechnologyNotExists:
+        raise HTTPException(status_code=404, detail="Technology Not Exists")
+    
+    except TechnologyUpdatingError:
+        raise HTTPException(status_code=500, detail="Database Error Updating Technology")
+
