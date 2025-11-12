18c30b1fe2eb0d95b92f2f60cb31e6d122ff7db9
Braian Torres <brianntorress.s@gmail.com>
2025-11-11 22:32:50 -0300

    feat(routes): implement add_portfolio() route

--- routes/portfolio.py
+++ routes/portfolio.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from database.db import get_session
+from database.tables import Portfolio
 from services.portfolio_service import PortfolioService
+from exceptions.exceptions import (PortfolioCreationError, PortfolioAlreadyExistsError)
 
 router = APIRouter()
 
@@ -15,3 +17,18 @@ def get_portfolio(session: Session = Depends(get_session)):
                 "data" : portfolio}
     
     raise HTTPException(status_code=404, detail="Portfolio Not Exists")
+
+@router.post("/portfolio", status_code=201)
+def add_portfolio(portfolio: Portfolio, session: Session = Depends(get_session)):
+    service = PortfolioService(session)
+    try:
+        portfolio = service.add_portfolio(portfolio)
+        if portfolio:
+            return {"success" : True,
+                    "data" : portfolio}
+    
+    except PortfolioAlreadyExistsError:
+        raise HTTPException(status_code=409, detail="Portfolio Already Exists")
+    except PortfolioCreationError:
+        raise HTTPException(status_code=500, detail="Error Creating Portfolio")
+        
