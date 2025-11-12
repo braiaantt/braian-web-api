8413c282e3a1766c610fde11c343cf3e1429eb50
Braian Torres <brianntorress.s@gmail.com>
2025-11-12 13:12:09 -0300

    feat(exceptions): add PortfolioNotExists() and PortfolioUpdatingError() exceptions and remove exceptions comments

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,7 +1,11 @@
-class PortfolioAlreadyExistsError(Exception):
-    """Raised when trying to create a portfolio that already exists."""
+class PortfolioAlreadyExistsError(Exception):   
     pass
 
 class PortfolioCreationError(Exception):
-    """Raised when an unexpected error occurs during portfolio creation."""
+    pass
+
+class PortfolioNotExists(Exception):
+    pass
+
+class PortfolioUpdatingError(Exception):
     pass
