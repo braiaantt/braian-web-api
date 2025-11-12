18d3327e7220abbb366e165c241e8f501e07b789
Braian Torres <brianntorress.s@gmail.com>
2025-11-12 17:32:54 -0300

    feat(routes): add delete endpoint for Technology resource

--- routes/technology_routes.py
+++ routes/technology_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from database import get_session, Technology
 from models import TechnologyUpdate
 from services import TechnologyService
-from exceptions import TechnologyNotExists, TechnologyCreationError, TechnologyUpdatingError
+from exceptions import TechnologyNotExists, TechnologyCreationError, TechnologyUpdatingError, TechnologyDeletingError
 
 router = APIRouter()
 
@@ -36,3 +36,17 @@ def update_technology(tech_id: int, data: TechnologyUpdate, session = Depends(ge
     except TechnologyUpdatingError:
         raise HTTPException(status_code=500, detail="Database Error Updating Technology")
 
+@router.delete("/technology/{tech_id}", status_code=20)
+def delete_technology(tech_id: int, session = Depends(get_session)):
+    service = TechnologyService(session)
+    try:
+        result = service.delete_technology(tech_id)
+        
+        if result:
+            return {"success" : True}
+    
+    except TechnologyNotExists:
+        raise HTTPException(status_code=404, detail="Technology Not Exists")
+    
+    except TechnologyDeletingError:
+        raise HTTPException(status_code=500, detail="Database Error Deleting Technology")
