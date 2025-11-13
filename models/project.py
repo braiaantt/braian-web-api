4a3a393ba6d68d20461dc22ee29232902d2de361
Braian Torres <brianntorress.s@gmail.com>
2025-11-13 18:00:34 -0300

    feat(models): create ProjectUpdate model

--- models/project.py
+++ models/project.py
@@ -1,7 +1,15 @@
 from sqlmodel import SQLModel
+from typing import Optional
 
 class PortfolioProject(SQLModel):
     id: int
     name: str
     small_about: str
     cover_src: str
+
+class ProjectUpdate(SQLModel):
+    name: Optional[str] = None
+    small_about: Optional[str] = None
+    big_about: Optional[str] = None
+    user_comment: Optional[str] = None
+    cover_src: Optional[str] = None
