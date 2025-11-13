9c8cb25dd4fc9fb8eac365274b25e2fbceb1b8dd
Braian Torres <brianntorress.s@gmail.com>
2025-11-13 17:58:20 -0300

    feat(daos): implement get_project(), insert_project(), delete_project() and update_project() methods to ProjectDao

--- daos/project_dao.py
+++ daos/project_dao.py
@@ -1,4 +1,4 @@
-from sqlmodel import Session, select
+from sqlmodel import Session, select, delete
 from sqlalchemy.exc import SQLAlchemyError
 from database.tables import Project
 from models.project import PortfolioProject
@@ -27,3 +27,51 @@ class ProjectDao:
         except SQLAlchemyError as error:
             print("Error Read Project: ", error)
             return []
+        
+    def get_project(self, project_id: int):
+        try:
+            project = self.session.exec(select(Project).where(Project.id == project_id))
+            if not project:
+                return None
+            
+            return project
+        
+        except SQLAlchemyError as error:
+            print("Error Read Project: ", error)
+            return None
+    
+    def insert_project(self, project: Project):
+        try:
+            self.session.add(project)
+            self.session.commit()
+            self.session.refresh(project)
+            return project
+        
+        except SQLAlchemyError as error:
+            print("Error Insert Porject: ", error)
+            self.session.rollback()
+            return None
+
+    def delete_project(self, project_id):
+        try:
+            statement = delete(Project).where(Project.id == project_id)
+            result = self.session.exec(statement)
+            self.session.commit()
+            return result > 0
+        
+        except SQLAlchemyError as error:
+            print("Error Delete Project: ", error)
+            self.session.rollback
+            return False
+        
+    def update_project(self, project: Project):
+        try:
+            #'project' is already attached to the session from TechnologyService
+            self.session.commit()
+            self.session.refresh(project)
+            return project
+        
+        except SQLAlchemyError as error:
+            print("Error Update Project: ", error)
+            self.session.rollback()
+            return None
