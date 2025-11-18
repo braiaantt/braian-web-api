62ea880dfc8eede49cb6cbd1ba9b8f16dc4c0086
Braian Torres <brianntorress.s@gmail.com>
2025-11-18 17:24:01 -0300

    feat(auth): add auth routes, /login and /refresh

---
+++ auth/auth_routes.py
@@ -0,0 +1,79 @@
+from fastapi import APIRouter, Depends, HTTPException
+from database import get_session
+from .admin import LoginData, AdminService, AdminNotExists, InvalidCredentials
+from .refresh_tokens import (RefreshTokenService, 
+                             RefreshTokenCreationError,RefreshTokenRevoked, RefreshTokenRevokingError)
+from .exceptions import TokenInvalidType, TokenExpired, TokenInvalidSignature
+from .jwt_utils import create_access_token, create_refresh_token, validate_signature_and_type, validate_exp_and_revoke
+
+
+router = APIRouter()
+
+@router.post("/login", status_code=201)
+def get_tokens(login_data: LoginData, session = Depends(get_session)):
+    admin_service = AdminService(session)
+    refresh_token_service = RefreshTokenService(session)
+
+    try:
+        admin = admin_service.get_admin(login_data)
+
+        if admin:
+            access_token = create_access_token(admin.id)
+            refresh_token = create_refresh_token(admin.id)
+            refresh_token_service.insert_refresh_token(refresh_token, admin.id)
+
+            return{
+                "access_token" : access_token,
+                "refresh_token" : refresh_token,
+                "token_type" : "bearer"
+            }
+            
+    except AdminNotExists:
+        raise HTTPException(status_code=404, detail="Admin Not Exists")
+    
+    except InvalidCredentials:
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
+    
+    except TokenInvalidType:
+        raise HTTPException(status_code=401, detail="Invalid Token Type")
+
+    except RefreshTokenCreationError:
+        raise HTTPException(status_code=500, detail="Database Error Creating Refresh Token")
+    
+
+@router.post("/refresh", status_code=201)
+def refresh_token(client_token: str, session = Depends(get_session)):
+    refresh_token_service = RefreshTokenService(session)
+    try:
+        validated_payload = validate_signature_and_type(client_token, "refresh")
+        exists = refresh_token_service.refresh_token_exists(validated_payload["sub"], client_token)
+
+        if not exists:
+            raise HTTPException(status_code=404, detail="Refresh Token Not Exists")
+        validate_exp_and_revoke(exists, validated_payload)
+        refresh_token_service.revoke_token(exists)
+
+        access_token = create_access_token(exists.admin_id)
+        refresh_token = create_refresh_token(exists.admin_id)
+        refresh_token_service.insert_refresh_token(refresh_token, exists.admin_id)
+
+        return{
+                "access_token" : access_token,
+                "refresh_token" : refresh_token,
+                "token_type" : "bearer"
+            }
+
+    except TokenInvalidSignature:
+        raise HTTPException(status_code=401, detail="Invalid Token Singature")
+    
+    except TokenInvalidType:
+        raise HTTPException(status_code=401, detail="Invalid Token Type")
+    
+    except TokenExpired:
+        raise HTTPException(status_code=404, detail="Refresh Token Expired")
+    
+    except RefreshTokenRevoked:
+        raise HTTPException(status_code=404, detail="Refresh Token Revoked")
+
+    except RefreshTokenRevokingError:
+        raise HTTPException(status_code=500, detail="Database Error Revoking Refresh Token")
