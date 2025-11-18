8869e6a3846060dad266a12225cadc6b8753f232
Braian Torres <brianntorress.s@gmail.com>
2025-11-18 17:18:18 -0300

    feat(auth): add AdminService

---
+++ auth/admin/admin_service.py
@@ -0,0 +1,19 @@
+from .admin_dao import AdminDao
+from sqlmodel import Session
+from .admin_model import LoginData
+from .admin_exceptions import AdminNotExists, InvalidCredentials
+from utils import verify_password
+
+class AdminService:
+    def __init__(self, session: Session):
+        self.admin_dao = AdminDao(session)
+
+    def get_admin(self, data: LoginData):
+        admin = self.admin_dao.get_admin(data.email)
+        if not admin:
+            raise AdminNotExists()
+        
+        if not verify_password(data.password, admin.password):
+            raise InvalidCredentials()
+
+        return admin
