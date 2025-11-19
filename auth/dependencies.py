7c425dbbd3d1a435443a368698d092907a92dc47
Braian Torres <brianntorress.s@gmail.com>
2025-11-19 12:33:38 -0300

    refactor(auth-dependencies): replace OAuth2PasswordBearer with HTTPBearer for simplicity

--- auth/dependencies.py
+++ auth/dependencies.py
@@ -1,13 +1,15 @@
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi import Depends, HTTPException
 from .jwt_utils import validate_access_token
 from .exceptions import TokenExpired, TokenInvalidSignature, TokenInvalidType
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+bearer_scheme = HTTPBearer()
 
 async def require_access_token(
-        token: str = Depends(oauth2_scheme),
+        credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
 ):
+    token = credentials.credentials
+    
     try:
         payload = validate_access_token(token)
         return payload
