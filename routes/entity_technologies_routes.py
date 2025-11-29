23a46504c00d01ad6ea6b717caa98ba71a4acd65
Braian Torres <brianntorress.s@gmail.com>
2025-11-28 23:39:19 -0300

    feat(routes): add router for EntityTechnology resource

---
+++ routes/entity_technologies_routes.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, HTTPException
+from models import EntityTechnologyData, EntityType, TechnologyRead
+from database import get_session
+from auth import require_access_token
+from services import EntityTechnologyService
+from exceptions import EntityTechnologyCreationError, EntityTechnologyRelationNotExists
+
+router = APIRouter()
+
+@router.get("/entity-technology", status_code=200, response_model=list[TechnologyRead])
+def get_relations(id_entity: int, type_entity: EntityType, session = Depends(get_session), _ = Depends(require_access_token)):
+    service = EntityTechnologyService(session)
+    try:
+        return service.get_relations(id_entity, type_entity)
+    except Exception:
+        raise HTTPException(status_code=404, detail="Entity Not Found")
+
+@router.post("/entity-technology", status_code=201, response_model=EntityTechnologyData)
+def relate_technology(data: EntityTechnologyData, session = Depends(get_session), _ = Depends(require_access_token)):
+    service = EntityTechnologyService(session)
+    try:
+        return service.relate_technology(data)
+    
+    except EntityTechnologyCreationError:
+        raise HTTPException(status_code=500, detail="Database Error Relating Entity With Technology")
+    
+@router.delete("/entity-technology", status_code=204)
+def delete_relation(id_entity: int, type_entity: EntityType, id_tech: int,
+                    session = Depends(get_session), _ = Depends(require_access_token)):
+    service = EntityTechnologyService(session)
+    try:
+        data = EntityTechnologyData(id_entity, type_entity, id_tech)
+        result = service.delete_relation(data)
+
+        if not result:
+            raise HTTPException(status_code=500, detail="Database Error Deleting Relation")
+        
+        return
+
+    except EntityTechnologyRelationNotExists:
+        raise HTTPException(status_code=404, detail="Relation With Technology Not Exists")
