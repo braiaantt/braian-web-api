1aacd720a7507d9d6739eb782420af92e3fc84e4
Braian Torres <brianntorress.s@gmail.com>
2025-11-28 23:39:59 -0300

    chore(routes): import router from entity_technology_routes to __init__.py module

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,2 +1,3 @@
 from .portfolio_routes import router as portfolio_router
 from .technology_routes import router as technology_router
+from .entity_technologies_routes import router as entity_technology_router
