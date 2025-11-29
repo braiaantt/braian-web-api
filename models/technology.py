8c40f4a7cff7e4de50973b10c13dcad9def9764b
Braian Torres <brianntorress.s@gmail.com>
2025-11-28 23:43:41 -0300

    feat(models): add TechnologyRead model

--- models/technology.py
+++ models/technology.py
@@ -1,5 +1,11 @@
 from sqlmodel import SQLModel
+from pydantic import BaseModel
 
 class TechnologyUpdate(SQLModel):
+    name: str
+    icon_src: str
+
+class TechnologyRead(BaseModel):
+    id: int
     name: str
     icon_src: str
