ff83a6c0d2fb30061c33d3b24efa55e02929e3d8
Braian Torres <brianntorress.s@gmail.com>
2025-11-29 12:53:02 -0300

    chore(models): add ProjectCreate and ProjectRead models

--- models/project.py
+++ models/project.py
@@ -14,3 +14,17 @@ class ProjectUpdate(BaseModel):
     big_about: Optional[str] = None
     user_comment: Optional[str] = None
     cover_src: Optional[str] = None
+
+class ProjectCreate(BaseModel):
+    name: str
+    small_about: str
+    big_about: str
+    user_comment: str
+
+class ProjectRead(BaseModel):
+    id: int
+    name: str
+    small_about: str
+    big_about: str
+    user_comment: str
+    cover_src: str
