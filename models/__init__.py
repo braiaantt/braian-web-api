ba4c84011579339feddd15ea5b0d7eb37a4192ae
Braian Torres <brianntorress.s@gmail.com>
2025-11-28 23:41:55 -0300

    chore(models): import EntityTechnologyData and EntityType to __init__.py module

--- models/__init__.py
+++ models/__init__.py
@@ -5,4 +5,8 @@ from .portfolio import (PortfolioProject,
                         PortfolioRead,
                         PortfolioUpdate)
 
-from .technology import TechnologyUpdate
+from .technology import TechnologyUpdate, TechnologyRead
+
+from .entity_technology import EntityTechnologyData
+
+from .entity_type import EntityType
