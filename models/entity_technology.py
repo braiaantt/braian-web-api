0c395996dd64fd1b29f9863f7cec315293b86d49
Braian Torres <brianntorress.s@gmail.com>
2025-12-01 07:03:48 -0300

    refactor(EntityTechnology): replace SQLModel with BaseModel

--- models/entity_technology.py
+++ models/entity_technology.py
@@ -1,7 +1,7 @@
-from sqlmodel import SQLModel
+from pydantic import BaseModel
 from .entity_type import EntityType
 
-class EntityTechnologyData(SQLModel):
+class EntityTechnologyData(BaseModel):
     id_entity: int
     type_entity: EntityType
     id_tech: int
