e5ff623ee0fda5a9d04eb427953bc5334922987a
Braian Torres <brianntorress.s@gmail.com>
2025-12-01 07:06:20 -0300

    refactor(ProjectModel): replace SQLModel with BaseModel and replace Technology and PortfolioProject with ProjectRead with TechnologyRead

--- models/portfolio.py
+++ models/portfolio.py
@@ -1,18 +1,18 @@
-from sqlmodel import SQLModel
+from pydantic import BaseModel
 from typing import Optional, List
-from database.tables import Technology
-from models.project import PortfolioProject
+from models.project import ProjectRead
+from models.technology import TechnologyRead
 
-class PortfolioUpdate(SQLModel):
+class PortfolioUpdate(BaseModel):
     user_name: Optional[str] = None
     user_profession: Optional[str] = None
     user_photo: Optional[str] = None
     user_about: Optional[str] = None
 
-class PortfolioRead(SQLModel):
+class PortfolioRead(BaseModel):
     user_name: str
     user_profession: str
     user_photo: str
     user_about: str
-    techs: List[Technology]
-    projects: List[PortfolioProject]
+    techs: List[TechnologyRead]
+    projects: List[ProjectRead]
