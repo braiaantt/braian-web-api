bed73549f19bc9bedef4835260f15811876c605b
Braian Torres <brianntorress.s@gmail.com>
2025-12-03 05:46:10 -0300

    refactor(EntityImageService): rename class to ProjectImageService and reimplement methods for new ProjectImageDao requirements

--- services/entity_image_service.py
+++ services/entity_image_service.py
@@ -1,41 +1,38 @@
 from sqlmodel import Session
-from daos.entity_image_dao import EntityImageDao
-from models.entity_image import ImageRelation, ImageRead
-from database.tables import Image
+from daos.entity_image_dao import ProjectImageDao
+from models.entity_image import ImageDelete
+from database.tables import ProjectImage
 from utils.file_manager import FileManager
 from exceptions.exceptions import EntityImageCreationError, EntityImageNotFound, EntityImageDeletingError
 from fastapi import UploadFile
-import os, json
+import os
 
-class EntityImageService:
+class ProjectImageService:
     def __init__(self, session: Session):
-        self.entity_image_dao = EntityImageDao(session)
+        self.project_image_dao = ProjectImageDao(session)
 
-    def get_image_paths(self, image_data: ImageRelation):
-        return self.entity_image_dao.get_image_paths(image_data)
+    def get_image_paths(self, id_project: int):
+        return self.project_image_dao.get_image_paths(id_project)
 
-    async def add_image(self, image_json: str, file: UploadFile):
-        data = json.loads(image_json)
-        image_data = ImageRelation(**data)
+    async def add_image(self, id_project: int, file: UploadFile):
         src_path = await FileManager.save_image(file, FileManager.PROJECT_FOLDER)
-        
-        image = Image(id_entity=image_data.entity_id, type_entity=image_data.entity_type, src=src_path)
-        image_created = self.entity_image_dao.add_image_relation(image)
+        image = ProjectImage(id_project=id_project, src=src_path)
+        image_created = self.project_image_dao.add_image_relation(image)
 
         if not image_created:
             raise EntityImageCreationError()
         
         return image_created.src
     
-    def delete_image(self, image_data: ImageRead):
-        image = self.entity_image_dao.get_image(image_data)
+    def delete_image(self, data: ImageDelete):
+        image = self.project_image_dao.get_image(data.id_project, data.src)
         if not image:
             raise EntityImageNotFound()
         
         src = image.src.lstrip("/") 
         os.remove(src)
         
-        success = self.entity_image_dao.remove_image_relation(image)
+        success = self.project_image_dao.remove_image(image)
         if not success:
             raise EntityImageDeletingError()
         
