c1b375c0bf8e49aba0ac57919726761a4eca2ecb
Braian Torres <brianntorress.s@gmail.com>
2025-12-03 23:27:30 -0300

    feat(models): add PortfolioProjectRead model

--- models/project.py
+++ models/project.py
@@ -1,5 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel
+from models.technology import TechnologyRead
+from models.feature import FeatureRead
+from models.technical_info import TechnicalInfoRead
 
 class ProjectUpdate(BaseModel):
     name: Optional[str] = None
@@ -15,6 +18,17 @@ class ProjectCreate(BaseModel):
     user_comment: str
 
 class ProjectRead(BaseModel):
+    id: int
+    name: str
+    small_about: str
+    big_about: str
+    user_comment: str
+    cover_src: str
+    techs: list[TechnologyRead]
+    feats: list[FeatureRead]
+    technical_info: list[TechnicalInfoRead]
+
+class PortfolioProjectRead(BaseModel):
     id: int
     name: str
     small_about: str
