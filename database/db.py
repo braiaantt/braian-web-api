79b2eaf50156f588f3aceccd663f2bd5c8a5194c
Braian Torres <brianntorress.s@gmail.com>
2025-12-05 14:16:34 -0300

    fix(db): enable SQLite foreign key constraints on engine connect

--- database/db.py
+++ database/db.py
@@ -1,7 +1,13 @@
 from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy import event
 
 DATABASE_URL = "sqlite:///database/database.db"
 engine = create_engine(DATABASE_URL, echo=True)
+
+def _fk_pragma_on_connect(dbapi_con, con_record):
+    dbapi_con.execute('pragma foreign_keys=ON')
+    
+event.listen(engine, 'connect', _fk_pragma_on_connect)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
